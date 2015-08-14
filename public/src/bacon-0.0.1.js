fc0bcc9db1e93a520706d21e8766ad38ac1fe00f
codingtwinky <twinky@codingtwinky.com>
2015-08-13 20:04:43 -0700

    Add front end ajax call

--- public/src/bacon-0.0.1.js
+++ public/src/bacon-0.0.1.js
@@ -1,14 +1,42 @@
 
 (function() {
   var metaKeyword = '';
+  var baconServerUrl = "http://127.0.0.1";
   var src = window.location.href;
   var metas = document.getElementsByTagName('meta');
   for (i in metas) {
-    console.log(metas[i]);
     if (metas[i].name && metas[i].name.indexOf('keyword') > -1) {
       metaKeyword += metas[i].content;
     }
   }
   
-  console.log(metaKeyword, src);
+  var divs = document.getElementsByClassName('baconLink');
+  
+  var defaultUrlArgs = "?metaKeyword=" + encodeURIComponent(metaKeyword);
+  defaultUrlArgs += "&src=" + encodeURIComponent(src);
+  
+  for (i in divs) {
+    var div = divs[i];
+    var args = defaultUrlArgs;
+    var width = div.getAttribute('width');
+    if (width) {
+      args += "&width=" + width;
+    }
+    var height = div.getAttribute('width');
+    if (height) {
+      args += "&height=" + height;
+    }
+    var keywords = div.getAttribute('keywords');
+    if (keywords) {
+      args += "&keywords=" + encodeURIComponent(keywords);
+    }
+    
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", baconServerUrl + "/getDonationLink" + args, true);
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        div.innerHTML = xmlhttp.responseText;
+      }
+    }
+  }
 })();
